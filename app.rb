184b165247e5f1c6e3e3c0c85b9ad1e4e88abe53
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-03-14 02:03:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,8 +7,12 @@ class App < Sinatra::Base
   end
 
   post '/team' do
+<<<<<<< HEAD
     # params[:name]
     # @name
+=======
+    @name
+>>>>>>> f5094a18c63e6eeb960acd4e0ec983cc0adf21de
     erb :team
   end
 
